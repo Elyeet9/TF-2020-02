07eb4ab183349498e6c8c38ab72a2f982a9c2280
Elyeet9 <goyasleonardo@gmail.com>
2020-11-02 16:05:05 -0500

    Cjuego
    las clases de imagenes y entidades terminadas creo

--- TrabajoFinal/CImagen.hpp
+++ TrabajoFinal/CImagen.hpp
@@ -1,6 +1,40 @@
 #pragma once
+
+using namespace System;
+using namespace System::Drawing;
+
 ref class CImagen
 {
-	System::Drawing::Rectangle^ gaa;
+protected:
+	System::Drawing::Rectangle area_dibujo;
+	Bitmap^ imagen;
+	short dx, dy;
+public:
+	CImagen(String^ ruta, System::Drawing::Rectangle area_dibujo)
+		: area_dibujo(area_dibujo) {
+		this->imagen = gcnew Bitmap(ruta);
+	}
+	~CImagen() {
+		delete this->imagen;
+	}
+	bool hay_colision(CImagen^ otro) {
+		return this->area_dibujo.IntersectsWith(otro->area_dibujo);
+	}
+	void renderizar(Graphics^ graficador) {
+		this->mover();
+		this->dibujar(graficador);
+	}
+	void set_direccion(short new_dx, short new_dy) {
+		this->dx = new_dx;
+		this->dy= new_dy;
+	}
+protected:
+	virtual void dibujar(Graphics^ graficador) {
+		graficador->DrawImage(this->imagen, this->area_dibujo);
+	}
+	void mover() {
+		this->area_dibujo.X += dx;
+		this->area_dibujo.Y += dy;
+	}
 };
 
--- TrabajoFinal/CJuego.hpp
+++ TrabajoFinal/CJuego.hpp
@@ -1,13 +1,31 @@
 #pragma once
 #include <Windows.h>
 #include <MMSystem.h>
-
+#include "CImagen.hpp"
+#include "CEntidad.hpp"
 
 ref class CJuego
 {
+	CImagen^ escenario;
+	CEntidad^ rey;
+	CEntidad^ aliado;
+	CEntidad^ corrupt;
+	CEntidad^ assassin;
 public:
 	CJuego(System::Drawing::Rectangle area_dibujo) {
+		escenario = gcnew CImagen("img\\fondo.png", area_dibujo);
+		rey = gcnew CEntidad("img\\rey.png", System::Drawing::Rectangle(0, 0, 100, 120), 4, 4, 5);
+		aliado = gcnew CEntidad("img\\aliado.png", System::Drawing::Rectangle(300, 300, 100, 120), 4, 4, 1);
+		corrupt = gcnew CEntidad("img\\corrupt.png", System::Drawing::Rectangle(600, 300, 100, 120), 4, 4, 1);
+		assassin = gcnew CEntidad("img\\assassin.png", System::Drawing::Rectangle(600, 700, 100, 120), 4, 4, 1);
 		PlaySound(TEXT("ost\\Step.wav"), NULL, SND_ASYNC | SND_FILENAME | SND_LOOP);
 	}
+	void renderizar(Graphics^ graficador) {
+		escenario->renderizar(graficador);
+		aliado->renderizar(graficador);
+		corrupt->renderizar(graficador);
+		assassin->renderizar(graficador);
+		rey->renderizar(graficador);
+	}
+	void mover_rey(short new_dx, short new_dy) { rey->set_direccion(new_dx, new_dy); }
 };
-
