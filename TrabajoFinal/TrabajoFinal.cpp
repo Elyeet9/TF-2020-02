00d5f2a0a9363abc55a7a8096fa73d7acbad49d3
Elyeet9 <goyasleonardo@gmail.com>
2020-11-02 13:32:44 -0500

    Creacion del proyecto

---
+++ TrabajoFinal/TrabajoFinal.cpp
@@ -0,0 +1,10 @@
+#include "pch.h"
+#include "MyForm.h"
+using namespace TrabajoFinal;
+using namespace System;
+
+int main(array<System::String ^> ^args)
+{
+    Application::Run(gcnew MyForm);
+    return 0;
+}
